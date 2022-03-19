cf8f80b2d4044c1628f214a312b8d950b2ce5cd9
Buzz <sheep52031@gmil.com>
2022-03-19 18:43:56 +0800

    -in RecipeModel build the function of getPortion to change RecipeDetail serving size. -button for recipe cards in featured view -display recipe detail when tapping on a featured card -move recipe title in between photo and serving size picker

--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -2,7 +2,6 @@
 //  RecipeDetailView.swift
 //  Recipe List App
 //
-//  Created by 林家任 on 2022/2/17.
 //
 
 import SwiftUI
@@ -11,50 +10,82 @@ struct RecipeDetailView: View {
     
     var recipe:Recipe
     
+    @State var selectedServingSize = 2
+    
     var body: some View {
-         
-        ScrollView{
-            
-            VStack(alignment:.leading){
+        
+        ScrollView {
+        
+            VStack (alignment: .leading) {
                 
                 // MARK: Recipe Image
-                Image(recipe.image).resizable().scaledToFill()
-            
-                //MARK: Ingredients
-                VStack(alignment:.leading){
+                Image(recipe.image)
+                    .resizable()
+                    .scaledToFill()
                 
-                    Text("Ingredients").font(.headline).padding([.top, .leading, .bottom])
-            
-                    ForEach(recipe.ingredients) {
-                        item in
-                        Text("• " + item.name)
+                // MARK: Recipe title
+                Text(recipe.name)
+                    .bold()
+                    .padding(.top, 20)
+                    .padding(.leading)
+                    .font(.largeTitle)
+                
+                
+                // MARK: Serving size picker
+                VStack (alignment: .leading) {
+                    Text("Select your serving size:")
+                    Picker("", selection: $selectedServingSize) {
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
                     }
-                    .padding(.horizontal)
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width:160)
                 }
+                .padding()
                 
-                //MARK: Divider
+                // MARK: Ingredients
+                VStack(alignment: .leading) {
+                    Text("Ingredients")
+                        .font(.headline)
+                        .padding([.bottom, .top], 5)
+                    
+                    ForEach (recipe.ingredients) { item in
+                        
+                        Text("• " + RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServings: selectedServingSize) + " " + item.name.lowercased())
+                    }
+                }
+                .padding(.horizontal)
+                
+                // MARK: Divider
                 Divider()
                 
-                //MARK: Directions
-                VStack(alignment:.leading){
-                    Text("Directions").font(.headline).padding([.top, .leading, .bottom],5)
+                // MARK: Directions
+                VStack(alignment: .leading) {
+                    Text("Directions")
+                        .font(.headline)
+                        .padding([.bottom, .top], 5)
                     
-                    ForEach(0..<recipe.directions.count,id:\.self){ index in
-                        Text(String(index+1)+". "+recipe.directions[index]).padding( .bottom,5)
+                    ForEach(0..<recipe.directions.count, id: \.self) { index in
+                        
+                        Text(String(index+1) + ". " + recipe.directions[index])
+                            .padding(.bottom, 5)
                     }
                 }
                 .padding(.horizontal)
             }
-        }.navigationBarTitle(recipe.name)
+            
+        }
     }
 }
 
-
 struct RecipeDetailView_Previews: PreviewProvider {
-        static var previews: some View {
-            // Create a dummy recipe and pass it into the detail view so that we can see a preview.
-            let model = RecipeModel()
+    static var previews: some View {
         
-            RecipeDetailView(recipe:model.recipes[0])
-        }
+        // Create a dummy recipe and pass it into the detail view so that we can see a preview
+        let model = RecipeModel()
+        
+        RecipeDetailView(recipe: model.recipes[0])
     }
+}
--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -4,66 +4,102 @@
 //
 //  Created by 林家任 on 2022/3/8.
 //
-
 import SwiftUI
 
 struct RecipeFeaturedView: View {
     
     @EnvironmentObject var model:RecipeModel
+    @State var isDetailViewShowing = false
+    @State var tabSelectionIndex = 0
     
     var body: some View {
-       
-        VStack(alignment:.leading,spacing: 0){
-            
-            Text("Featured Recipe")
+        
+        VStack(alignment: .leading, spacing: 0) {
+        
+            Text("Featured Recipes")
                 .bold()
                 .padding(.leading)
-                .padding(.top,40)
+                .padding(.top, 40)
                 .font(.largeTitle)
-            
-            GeometryReader{ geo in
-            
-            TabView{
                 
-                ForEach(0..<model.recipes.count){ index in
+            
+            GeometryReader { geo in
+            
+            TabView (selection: $tabSelectionIndex) {
+                
+                // Loop through each recipe
+                ForEach (0..<model.recipes.count) { index in
                     
-                    //Only show those that should be feature
+                    // Only show those that should be featured
                     if model.recipes[index].featured == true {
-                
-                        //Recipe card
-                        ZStack{
-                            Rectangle().foregroundColor(.white)
+                    
+                        // Recipe card button
+                        Button(action: {
                             
-                            VStack(spacing:0){
-                                Image(model.recipes[index].image)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .clipped()
-                                Text(model.recipes[index].name)
-                                    .padding(5)
+                            // Show the recipe detail sheet
+                            self.isDetailViewShowing = true
+                            
+                        }, label: {
+                            
+                            // Recipe card
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    
+                                
+                                VStack(spacing: 0) {
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    Text(model.recipes[index].name)
+                                        .padding(5)
+                                }
                             }
-                        }
-                        .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center).cornerRadius(15).shadow(color:Color(.sRGB,red:0,green:0,blue:0,opacity: 0.5),radius: 10,x:-5,y:5)
                             
+                        })
+                        .sheet(isPresented: $isDetailViewShowing) {
+                            // Show the Recipe Detail View
+                            RecipeDetailView(recipe: model.recipes[index])
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(15)
+                        .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
+                        
                     }
                 }
+                
             }
             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode:.always))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            
         }
         
-            VStack(alignment:.leading,spacing:10){
-                Text("Preparation Time:").font(.headline)
-                Text("1 hour")
-                Text("Hightlights")
+            VStack (alignment: .leading, spacing: 10) {
+                
+                Text("Preparation Time:")
                     .font(.headline)
-                Text("Healthy Hearty")
+                
+                Text("Highlights")
+                    .font(.headline)
             }
-            .padding([.leading,.bottom])
+            .padding([.leading, .bottom])
         }
+        .onAppear(perform: {
+            setFeaturedIndex()
+        })
+    }
+    
+    func setFeaturedIndex() {
+        
+        // Find the index of first recipe that is featured
+        let index = model.recipes.firstIndex { (recipe) -> Bool in
+            return recipe.featured
+        }
+        tabSelectionIndex = index ?? 0
     }
 }
-
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
